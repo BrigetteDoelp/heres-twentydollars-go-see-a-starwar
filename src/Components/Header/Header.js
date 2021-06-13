76b351c7e83a56df990c00528fbe628af8f3f138
Brigette Doelp <brigette.doelp@gmail.com>
2021-06-13 00:42:26 -0600

    Populate top two nav bars with content according to comp

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,9 +8,35 @@ import flag from '../../Assets/united-states.png';
 
 function Header() {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <article className='header'>
+      <section className='top'>
+        <p className='shippingNote'>Free shipping. Free returns.</p>
+        <nav className='topNavBar'>
+          <p>Login</p>
+          <p>Join</p>
+          <p>UGG Rewards</p>
+          <img className='languageIndicator' src={flag} />
+        </nav>
+      </section>
+      <section className='middle'>
+        <div className='padding'> </div>
+        <nav className='middleNavBar'>
+          <img className ='littleUggLogo storeSections' src={uggLogo} />
+          <p className='storeSections'>WOMEN</p>
+          <p className='storeSections'>MEN</p>
+          <p className='storeSections'>KIDS</p>
+          <p className='storeSections'>HOME</p>
+          <p className='storeSections'>SALE</p>
+          <p className='storeSections'>EXPLORE</p>
+          <p className='storeSections'>STORES</p>
+          <img className ='searchIcon storeSections' src={search} />
+          <img className ='cartIcon storeSections' src={cart} />
+        </nav>
+        <div className='padding'> </div>
+      </section>
+      <section className='bottom'>
+      </section>
+    </article>
   );
 }
 
