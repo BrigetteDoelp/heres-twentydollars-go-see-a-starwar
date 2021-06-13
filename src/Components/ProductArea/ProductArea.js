982ba15f3511ed1e19cbd84921a83f31c14a5088
Brigette Doelp <brigette.doelp@gmail.com>
2021-06-12 22:24:18 -0600

    Fix file names for image imports

--- src/Components/ProductArea/ProductArea.js
+++ src/Components/ProductArea/ProductArea.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './ProductArea.css';
-import boot1back from '../../Assets/boots_1_back_x.png';
-import boot1front from '../../Assets/boots_1_front_x.png';
-import boot2back from '../../Assets/boots_2_back_x.png';
-import boot2front from '../../Assets/boots_2_front_x.png';
+import boot1back from '../../Assets/boots_1__back__x.png';
+import boot1front from '../../Assets/boots_1__front__x.png';
+import boot2back from '../../Assets/boots_2__back__x.png';
+import boot2front from '../../Assets/boots_2__front__x.png';
 import goblinShoes from '../../Assets/goblin_shoes.png';
 
 function ProductArea() {
