8cb85262581f0bb2ace2d84dc02f2872070ddf79
Brigette Doelp <brigette.doelp@gmail.com>
2021-06-12 22:32:43 -0600

    Add final assets for header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Header.css';
-import uggLogo from '../../Assets/ugg_logo.png'
+import uggLogo from '../../Assets/ugg_logo.png';
+import cart from '../../Assets/cart.png';
+import search from '../../Assets/search.png';
+import flag from '../../Assets/united-states.png';
+
 
 function Header() {
   return (
