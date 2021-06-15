6a4edf1b4205d1a1dbb9941fe4419e193a56c61c
Brigette Doelp <brigette.doelp@gmail.com>
2021-06-14 20:27:02 -0600

    Refactor component for use of warpspeed package

--- src/Components/ProductIntro/ProductIntro.js
+++ src/Components/ProductIntro/ProductIntro.js
@@ -1,17 +1,46 @@
 import React from 'react';
+import { Component } from 'react';
 import './ProductIntro.css';
 import bootOverlay from '../../Assets/hyperdrive_overlay.png';
 import rewardsLogo from '../../Assets/ugg_rewards.png';
+import WarpSpeed from './warpspeed.js';
 
-function ProductIntro() {
-  return (
-    <article className='productIntro'>
-      <img className='bootOverlay' src={bootOverlay} />
-      <img className='rewardsLogo' src={rewardsLogo} />
-      <h3 className='rewardsTitle'>UGG REWARDS PERK.</h3>
-      <p className='perkDescription'>AS AN UGG REWARDS MEMBER, YOU GET EXCLUSIVE ACCESS TO THESE UGG X STAR WARS BOOTS.</p>
-    </article>
-  );
+class ProductIntro extends Component {
+  constructor() {
+    super();
+    this.state = {
+      iNeedThisIdk: true,
+    }
+  }
+
+  componentDidMount() {
+    const x = new WarpSpeed('warpSpeed')
+  }
+
+  render() {
+    return (
+      <article className='productIntro'>
+        <img className='bootOverlay' src={bootOverlay} />
+        <canvas id='warpSpeed'> </canvas>
+        <img className='rewardsLogo' src={rewardsLogo} />
+        <h3 className='rewardsTitle'>UGG REWARDS PERK.</h3>
+        <p className='perkDescription'>AS AN UGG REWARDS MEMBER, YOU GET EXCLUSIVE ACCESS TO THESE UGG X STAR WARS BOOTS.</p>
+      </article>
+    );
+  }
 }
+
+
+
+// function ProductIntro() {
+//   return (
+//     <article className='productIntro'>
+//       <img className='bootOverlay' src={bootOverlay} />
+//       <img className='rewardsLogo' src={rewardsLogo} />
+//       <h3 className='rewardsTitle'>UGG REWARDS PERK.</h3>
+//       <p className='perkDescription'>AS AN UGG REWARDS MEMBER, YOU GET EXCLUSIVE ACCESS TO THESE UGG X STAR WARS BOOTS.</p>
+//     </article>
+//   );
+// }
 
 export default ProductIntro;
