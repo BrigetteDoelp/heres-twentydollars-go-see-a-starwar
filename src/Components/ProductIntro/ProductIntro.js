5555d618f29e8e8a837b3ed717c41369f347eabc
Brigette Doelp <brigette.doelp@gmail.com>
2021-06-14 18:23:29 -0600

    Convert functional component into class component to accomidate animation functionality

--- src/Components/ProductIntro/ProductIntro.js
+++ src/Components/ProductIntro/ProductIntro.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Component } from 'react'
 import './ProductIntro.css';
 import bootOverlay from '../../Assets/hyperdrive_overlay.png';
 import rewardsLogo from '../../Assets/ugg_rewards.png';
+import warpSpeedAnimation from './warpSpeedAnimation.js';
 
-function ProductIntro() {
-  return (
-    <article className='productIntro'>
-      <img className='bootOverlay' src={bootOverlay} />
-      <img className='rewardsLogo' src={rewardsLogo} />
-      <h3 className='rewardsTitle'>UGG REWARDS PERK.</h3>
-      <p className='perkDescription'>AS AN UGG REWARDS MEMBER, YOU GET EXCLUSIVE ACCESS TO THESE UGG X STAR WARS BOOTS.</p>
-    </article>
-  );
+class ProductIntro extends Component {
+  constructor() {
+    super()
+    this.state = {
+      warp: true,
+    }
+  }
+
+
+  render() {
+
+    return (
+      <article className='productIntro'>
+        <img className='bootOverlay' src={bootOverlay} />
+        <canvas id='warpSpeed'> </canvas>
+        <img className='rewardsLogo' src={rewardsLogo} />
+        <h3 className='rewardsTitle'>UGG REWARDS PERK.</h3>
+        <p className='perkDescription'>AS AN UGG REWARDS MEMBER, YOU GET EXCLUSIVE ACCESS TO THESE UGG X STAR WARS BOOTS.</p>
+      </article>
+    );
+  }
 }
+
 
 export default ProductIntro;
